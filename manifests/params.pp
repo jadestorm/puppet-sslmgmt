2d78d1e55d735cbc6504695107a7ac91c9b88d31
Daniel Henninger <daniel@ncsu.edu>
2019-05-30 14:32:31 -0400

    Fix up spacing.

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,39 +15,39 @@
 # @License Apache-2.0 <http://spdx.org/licenses/Apache-2.0>
 #
 class sslmgmt::params {
-    case $::osfamily {
-        'RedHat': {
-            $pkistore = {
-               'default'      => {
-                   'certpath' => '/etc/pki/tls/certs',
-                   'keypath'  => '/etc/pki/tls/private',
-                   'certmode' => '0644',
-                   'owner'    => 'root',
-                   'group'    => 'root',
-                },
-            }
-        }
-        'Debian': {
-            $pkistore = {
-                'default'      => {
-                    'certpath' => '/etc/ssl/certs',
-                    'keypath'  => '/etc/ssl/private',
-                    'certmode' => '0644',
-                    'owner'    => 'root',
-                    'group'    => 'root',
-                 },
-            }
-        }
-        default: {
-            $pkistore = {
-                 'default'      => {
-                     'certpath' => '/etc/ssl/certs',
-                     'keypath'  => '/etc/ssl/private',
-                     'certmode' => '0644',
-                     'owner'    => 'root',
-                     'group'    => 'root',
-                 },
-            }
-        }
+  case $::osfamily {
+    'RedHat': {
+      $pkistore = {
+        'default'    => {
+          'certpath' => '/etc/pki/tls/certs',
+          'keypath'  => '/etc/pki/tls/private',
+          'certmode' => '0644',
+          'owner'  => 'root',
+          'group'  => 'root',
+        },
+      }
     }
+    'Debian': {
+      $pkistore = {
+        'default'    => {
+          'certpath' => '/etc/ssl/certs',
+          'keypath'  => '/etc/ssl/private',
+          'certmode' => '0644',
+          'owner'  => 'root',
+          'group'  => 'root',
+        },
+      }
+    }
+    default: {
+      $pkistore = {
+        'default'    => {
+          'certpath' => '/etc/ssl/certs',
+          'keypath'  => '/etc/ssl/private',
+          'certmode' => '0644',
+          'owner'  => 'root',
+          'group'  => 'root',
+        },
+      }
+    }
+  }
 }
